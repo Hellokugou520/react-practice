da0067a0fc7f947a11b92d6ecdf1b70c88cab6d0
Bpoi <1378400943@qq.com>
2021-11-10 22:48:55 +0800

    todoList-初始化列表数据

--- 14.react脚手架/react_staging/src/App.jsx
+++ 14.react脚手架/react_staging/src/App.jsx
@@ -5,12 +5,21 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 export default class App extends Component {
+  state = {
+    todoList: [
+      { id: "1", name: "学习", done: true },
+      { id: "2", name: "玩游戏", done: true },
+      { id: "3", name: "阅读", done: false },
+    ],
+  };
+
   render() {
+    const { todoList } = this.state;
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header />
-          <List />
+          <List todoList={todoList} />
           <Footer />
         </div>
       </div>
--- 14.react脚手架/react_staging/src/components/List/index.jsx
+++ 14.react脚手架/react_staging/src/components/List/index.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Item from "../Item";
-import './index.css'
+import "./index.css";
 
 export default class List extends Component {
   render() {
+    const { todoList } = this.props;
     return (
       <ul className="todo-main">
-        <Item />
+        {todoList.map((item) => {
+          return <Item key={item.id} {...item} />;
+        })}
       </ul>
     );
   }
