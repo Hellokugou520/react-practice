1b32c08ee6ae91a9e0e12344cdf09ca153c7b429
Bpoi <1378400943@qq.com>
2021-11-11 16:02:57 +0800

    list交互效果

--- 14.react脚手架/react_staging/src/components/Item/index.jsx
+++ 14.react脚手架/react_staging/src/components/Item/index.jsx
@@ -2,18 +2,40 @@ import React, { Component } from "react";
 import "./index.css";
 
 export default class Item extends Component {
+  state = { flag: false };
+
   render() {
+    const { flag } = this.state;
     const { name, done } = this.props;
     return (
-      <li>
+      <li
+        onMouseEnter={() => {
+          this.enter(true);
+        }}
+        onMouseLeave={() => {
+          this.leave(false);
+        }}
+        style={{ backgroundColor: flag ? "#ccc" : "#fff" }}
+      >
         <label>
           <input type="checkbox" defaultChecked={done} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: "none" }}>
+        <button
+          className="btn btn-danger"
+          style={{ display: flag ? "block" : "none" }}
+        >
           删除
         </button>
       </li>
     );
   }
+
+  enter = (flag) => {
+    this.setState({ flag: !this.state.flag });
+  };
+
+  leave = (flag) => {
+    this.setState({ flag: !this.state.flag });
+  };
 }
