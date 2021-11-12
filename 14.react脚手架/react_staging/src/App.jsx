d1b3248406a022d209eb1753c6be589729ae4f4a
Bpoi <1378400943@qq.com>
2021-11-12 17:00:12 +0800

    配置跨域请求代理

--- 14.react脚手架/react_staging/src/App.jsx
+++ 14.react脚手架/react_staging/src/App.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class App extends Component {
   render() {
-    return <div>App</div>;
+    return <button onClick={this.getStudent}>点击获取学生数据</button>;
   }
+
+  getStudent = () => {
+    axios.get("http://localhost:3000/students").then(
+      (res) => {
+        console.log("成功", res.data);
+      },
+      (err) => {
+        console.log("失败", err);
+      }
+    );
+  };
 }
