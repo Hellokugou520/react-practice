fa38318d019960b5fcfda6156310790d8bfdb8b4
Bpoi <1378400943@qq.com>
2021-11-13 13:22:10 +0800

    配置代理

--- 14.react脚手架/react_staging/src/App.jsx
+++ 14.react脚手架/react_staging/src/App.jsx
@@ -3,11 +3,26 @@ import axios from "axios";
 
 export default class App extends Component {
   render() {
-    return <button onClick={this.getStudent}>点击获取学生数据</button>;
+    return (
+      <div>
+        <button onClick={this.getStudent}>点击获取学生数据</button>
+        <button onClick={this.getCars}>点击获取汽车数据</button>
+      </div>
+    );
   }
 
   getStudent = () => {
-    axios.get("http://localhost:3000/students").then(
+    axios.get("http://localhost:3000/api1/students").then(
+      (res) => {
+        console.log("成功", res.data);
+      },
+      (err) => {
+        console.log("失败", err);
+      }
+    );
+  };
+  getCars = () => {
+    axios.get("http://localhost:3000/api2/cars").then(
       (res) => {
         console.log("成功", res.data);
       },
