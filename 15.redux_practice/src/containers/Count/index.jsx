1b9a25eeae6ea4fed427fdaa28db4bb5a5f627e4
Bpoi <1378400943@qq.com>
2021-12-01 17:41:08 +0800

    react-redux数据共享：编写Person组件

---
+++ 15.redux_practice/src/containers/Count/index.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+// 引入connect用于连接UI组件与redux
+import { connect } from "react-redux";
+import {
+  addAction,
+  reduceAction,
+  addAsyncAction,
+} from "../../redux/actions/count";
+
+class Count extends Component {
+  add = () => {
+    const { value } = this.numSelect;
+    this.props.add(value * 1);
+  };
+  reduce = () => {
+    const { value } = this.numSelect;
+    this.props.reduce(value * 1);
+  };
+  addIfOdd = () => {
+    const { value } = this.numSelect;
+    if (this.props.count % 2 !== 0) {
+      this.props.add(value * 1);
+    }
+  };
+  addAsync = () => {
+    const { value } = this.numSelect;
+    this.props.addAsync(value * 1, 500);
+  };
+  render() {
+    return (
+      <div>
+        <h1>Count组件</h1>
+        <h2>当前求和为：{this.props.count}</h2>
+        <select
+          ref={(e) => {
+            this.numSelect = e;
+          }}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        &nbsp;
+        <button onClick={this.add}>+</button>&nbsp;
+        <button onClick={this.reduce}>-</button>&nbsp;
+        <button onClick={this.addIfOdd}>奇数+</button>&nbsp;
+        <button onClick={this.addAsync}>异步+</button>
+      </div>
+    );
+  }
+}
+
+// 使用connect()()创建并暴露一个Count的容器组件
+export default connect((state) => ({ count: state }), {
+  add: addAction,
+  reduce: reduceAction,
+  addAsync: addAsyncAction,
+})(Count);
---
+++ 15.redux_practice/src/containers/Person/index.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export default class Person extends Component {
+  add = () => {
+    const name = this.name.value;
+    const age = this.age.value;
+    console.log(name, age);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Person组件</h1>
+        <input
+          ref={(el) => (this.name = el)}
+          type="text"
+          placeholder="请输入姓名"
+        />
+        &nbsp;
+        <input
+          ref={(el) => (this.age = el)}
+          type="text"
+          placeholder="请输入年龄"
+        />
+        &nbsp;
+        <button onClick={this.add}>添加</button>
+        <ul>
+          <li>姓名1--年龄1</li>
+          <li>姓名2--年龄2</li>
+          <li>姓名3--年龄3</li>
+        </ul>
+      </div>
+    );
+  }
+}
