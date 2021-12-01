b4974e9ee97eaeb85f928e3f7d3a707f5a055884
Bpoi <1378400943@qq.com>
2021-12-01 17:03:20 +0800

    react-redux优化-整合UI组件和容器组件

--- 15.redux_practice/06.react-redux的优化/containers/Count/index.jsx
+++ 15.redux_practice/06.react-redux的优化/containers/Count/index.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+// 引入connect用于连接UI组件与redux
+import { connect } from "react-redux";
+import {
+  addAction,
+  reduceAction,
+  addAsyncAction,
+} from "../../redux/count_action";
 
-export default class Count extends Component {
+class Count extends Component {
   add = () => {
     const { value } = this.numSelect;
     this.props.add(value * 1);
@@ -41,3 +48,21 @@ export default class Count extends Component {
     );
   }
 }
+
+// 使用connect()()创建并暴露一个Count的容器组件
+export default connect(
+  (state) => ({ count: state }),
+  // mapDispatchToProps的一般写法
+  // (dispatch) => ({
+  //   add: (data) => dispatch(addAction(data)),
+  //   reduce: (data) => dispatch(reduceAction(data)),
+  //   addAsync: (data, time) => dispatch(addAsyncAction(data, time)),
+  // })
+
+  // mapDispatchToProps的简写
+  {
+    add: addAction,
+    reduce: reduceAction,
+    addAsync: addAsyncAction,
+  }
+)(Count);
