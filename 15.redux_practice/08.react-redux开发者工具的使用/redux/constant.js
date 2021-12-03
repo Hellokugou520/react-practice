bcd8710fdaa333ff66f806d6e00a2f8530536f7c
Bpoi <1378400943@qq.com>
2021-12-03 11:36:38 +0800

    react-redux开发者工具

---
+++ 15.redux_practice/08.react-redux开发者工具的使用/redux/constant.js
@@ -0,0 +1,6 @@
+/**
+ * 该模块用于定义action对象中type类型的常量值，目的是为了便于管理的同时防止单词写错
+ */
+export const ADD = 'add'
+export const REDUCE = 'reduce'
+export const ADD_PERSON = 'addPerson'
---
+++ 15.redux_practice/08.react-redux开发者工具的使用/redux/store.js
@@ -0,0 +1,13 @@
+/**
+ * 该文件专门用于暴露一个store对象，整个应用只有一个store对象
+ */
+// 引入createStore，专门用于创建redux中最为核心的store对象
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+// 如果需要用Redux DevTools插件在浏览器中调试，则需要引入composeWithDevTools
+import { composeWithDevTools } from 'redux-devtools-extension'
+// 引入redux-thunk，用于支持异步action
+import thunk from 'redux-thunk'
+import countReducer from './reducers/count'
+import personReducer from './reducers/person'
+
+export default createStore(combineReducers({ countReducer, personReducer }), composeWithDevTools(applyMiddleware(thunk)))
