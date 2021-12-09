a69bedf44537d0ff29d2b33ce0a992900fc51357
Bpoi <1378400943@qq.com>
2021-12-09 21:28:25 +0800

    hooks-useEffect

--- 16.react_extension/src/components/3_hooks/index.jsx
+++ 16.react_extension/src/components/3_hooks/index.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 export default function Demo() {
   const [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+    let timer = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
   function add() {
     setCount((count) => count + 1);
+  }
+  function death() {
+    ReactDOM.unmountComponentAtNode(document.getElementById("root"));
   }
   return (
     <div>
       <h2>当前次数：{count}</h2>
       <button onClick={add}>次数+1</button>
+      <button onClick={death}>销毁组件</button>
     </div>
   );
 }
