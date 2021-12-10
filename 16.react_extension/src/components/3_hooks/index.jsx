22152b2c9ca545f3444724488e65329074813d4b
Bpoi <1378400943@qq.com>
2021-12-10 15:04:38 +0800

    hooks-useRef

--- 16.react_extension/src/components/3_hooks/index.jsx
+++ 16.react_extension/src/components/3_hooks/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 export default function Demo() {
   const [count, setCount] = React.useState(0);
+  const myInput = React.useRef();
   React.useEffect(() => {
     let timer = setInterval(() => {
       setCount((count) => count + 1);
@@ -17,11 +18,16 @@ export default function Demo() {
   function death() {
     ReactDOM.unmountComponentAtNode(document.getElementById("root"));
   }
+  function show() {
+    alert(myInput.current.value);
+  }
   return (
     <div>
       <h2>当前次数：{count}</h2>
+      <input type="text" ref={myInput} />
       <button onClick={add}>次数+1</button>
       <button onClick={death}>销毁组件</button>
+      <button onClick={show}>展示输入框数据</button>
     </div>
   );
 }
